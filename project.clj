ffba2d142ded764ee04b85bd42ba4fad4a8af8c6
Robin Kraft <rkraft4@gmail.com>
2013-03-04 17:10:04 -0800

    Updated deps for Clojure 1.4 and AWS JDK 1.3.1 (excluding finicky dep for the latter)

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject clj-aws "0.0.1-SNAPSHOT"
+(defproject clj-aws "0.0.2-SNAPSHOT"
   :description "Clojure wrapper for the AWS Java SDK"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.2.6"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [aws-java-sdk "1.3.1"
+                  :exclusions [org.codehaus.jackson/jackson-core-asl]]])
